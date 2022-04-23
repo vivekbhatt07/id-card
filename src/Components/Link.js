ef1909d21a878bf1acf8dae37808d40242b65d1d
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-04-23 23:28:02 +0530

    Header styled

--- src/Components/Link.js
+++ src/Components/Link.js
@@ -6,12 +6,16 @@ export default function Link() {
   return (
     <div className="link">
       <button className="link__btn">
-        <img className="link__img" src={Email} alt="mail" />
-        <span className="link__text">Email</span>
+        <div className="link__content">
+          <img className="link__img" src={Email} alt="mail" />
+          <span className="link__text">Email</span>
+        </div>
       </button>
-      <button className="link__btn">
-        <img className="link__img" src={Linkedin} alt="linkedin" />
-        <span className="link__text">LinkedIn</span>
+      <button className="link__btn link__btn--active">
+        <div className="link__content">
+          <img className="link__img" src={Linkedin} alt="linkedin" />
+          <span className="link__text">LinkedIn</span>
+        </div>
       </button>
     </div>
   );
